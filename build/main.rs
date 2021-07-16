72e9c049ac83ffeb568085ee24c9948627838ace
Steven Stanfield <stanfield@scarecrowtech.com>
2021-07-16 22:16:30 +0000

    Add serde support to the protobuf structs.

--- build/main.rs
+++ build/main.rs
@@ -129,6 +129,7 @@ pub fn main() {
     prost_build::Config::new()
         .out_dir(proto_out)
         //        .compile_well_known_types()
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile_protos(&protos, &[out_dir.clone()])
         .unwrap();
 
